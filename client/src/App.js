a295615e281f31952ce8c7e990f382132c9759fa
nicholasbilotto <96328916+nicholasbilotto@users.noreply.github.com>
2024-03-26 13:11:52 -0700

    Remove Login

--- client/src/App.js
+++ client/src/App.js
@@ -20,17 +20,17 @@ import DataTable from "scenes/datatable";
 function App() {
 	const mode = useSelector((state) => state.global.mode);
 	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
 	return (
 		<div className="app">
 			<BrowserRouter>
 				<ThemeProvider theme={theme}>
 					<CssBaseline />
 					<Routes>
-						<Route path="/login" element={<Login />} />
 						<Route element={<Layout />}>
 							<Route
 								path="/"
-								element={<Navigate to="/login" replace />}
+								element={<Navigate to="/dashboard" replace />}
 							/>
 							<Route path="/dashboard" element={<Dashboard />} />
 							<Route path="/overview" element={<Overview />} />
